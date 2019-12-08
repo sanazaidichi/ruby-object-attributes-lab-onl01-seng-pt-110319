dfe7ced7e16b358c1fc82103e8643cf5d39be88b
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-12-08 08:28:16 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person
+  
+  def pname=(persons_name)
+    @name = persons_name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def job=(persons_name)
+    @job = persons_name
+  end
+  
+  def job
+    @job 
+  end 
+  
+end
+
+ 
+beyonce = name.new
+beyonce.name = "Beyonce"
+
+
+
+puts beyonce.name 
